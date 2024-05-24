c79b0340c5afc95bd5cda6b54779377b3fb8607e
Chemaclass <chemaclass@outlook.es>
2024-05-24 11:52:23 +0200

    remove unnecesary src and test dir from phel-config

--- phel-config.php
+++ phel-config.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 use Phel\Config\PhelConfig;
 
 return (new PhelConfig())
-    ->setSrcDirs(['src'])
-    ->setTestDirs(['tests'])
     ->setIgnoreWhenBuilding([
         'performance.phel',
         'local.phel'
-    ]);
+    ])
+;
